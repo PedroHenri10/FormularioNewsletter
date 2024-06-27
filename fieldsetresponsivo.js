46db1d296e18bb0fc8802d6c96e49d196ccb407a
PedroHenri10 <dinhonoliver@gmail.com>
2024-06-26 22:31:33 -0300

    js desenolvido mas nao funcionou

---
+++ fieldsetresponsivo.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+
+    const mediaQuery = window.matchMedia('(max-width: 614px)');
+    const fieldesetTechNews = document.getElementsByTagName("fieldset")
+    const originalFieldset = fieldesetTechNews.innerHTML;
+
+    function handleMediaQuery(e) {
+        if (e.matches) {
+            const fieldsetInCellphone = `<fieldset>
+            <legend>What are your interests:</legend>
+
+            <label for="Consoles">Consoles</label>
+            <input type="checkbox" name="consoles" id="consoles" value="consoles" class="input_ckeckbox">
+            
+            <label for="cellphone">Cellphone</label>
+            <input type="checkbox" name="cellphone" id="cellphone" value="cellphone" class="input_ckeckbox"><br>
+
+            <label for="computer_laptop">Computer/laptop</label>
+            <input type="checkbox" name="computer_laptop" id="computer_laptop" value="computer_laptop" class="input_ckeckbox">
+
+            <label for="peripherals">Peripherals</label>
+            <input type="checkbox" name="peripherals" id="peripherals" value="peripherals" class="input_ckeckbox"><br>
+
+            <label for="companies_softwares">Companies and softwares</label>
+            <input type="checkbox" name="companies_softwares" id="companies_softwares" value="companies_softwares" class="input_ckeckbox">
+
+            <label for="others">Others</label>
+            <input type="checkbox" name="others" id="others" value="others" class="input_ckeckbox">
+            </fieldset>`;
+            fieldesetTechNews.innerHTML = fieldsetInCellphone;
+        }else{
+            fieldesetTechNews.innerHTML = originalFieldset;
+        }
+    }
+
+    handleMediaQuery(mediaQuery);
+
+    mediaQuery.addListener(handleMediaQuery);
+
+}
